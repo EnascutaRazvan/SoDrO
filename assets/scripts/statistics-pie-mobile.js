0988e9ba6afdc639971adf577d8ee05e9c8ebed7
cezar <79227201+FrentescuCezar@users.noreply.github.com>
2022-06-20 23:16:32 +0300

    Update Mare

---
+++ assets/scripts/statistics-pie-mobile.js
@@ -0,0 +1,46 @@
+anychart.onDocumentReady(function () {
+
+    // set the data
+    var data = [
+
+        { x: "Soda", value: 20 },
+        { x: "Dairy Bevereage", value: 30 },
+        { x: "Energy Drink", value: 10 },
+        { x: "Syrup", value: 50 },
+        { x: "Tea", value: 20 },
+
+    ];
+
+    // create a pie chart and set the data
+    chart = anychart.pie(data);
+
+
+    var palette = anychart.palettes.rangeColors();
+    palette.items([{ color: '#ffab19' }, { color: '#ffce79' },
+    { color: '#9656c3' }, { color: '#c887ff' },
+    { color: '#822121' },]);
+    // set chart palette
+    chart.palette(palette);
+
+
+    chart.labels().position("outside");
+    chart.labels().format(function () {
+        var percentOfTotal = (this.getData("value") * 100) / this.getStat("sum");
+
+        return "<span style='color:#663399;font-size:20px'>" + this.x + ": " + percentOfTotal.toFixed(1) + "%";
+    })
+    chart.labels().useHtml(true);
+    chart.labels().format()
+
+
+    chart
+        .legend(false)
+        .radius(150)
+        .startAngle(80)
+        .innerRadius(65);
+
+    chart.container('statistics-pie');
+
+    chart.draw();
+
+});
